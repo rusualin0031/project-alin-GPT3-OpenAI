7223bafa33b6c2d7ae3401c1ff38e0f9fc9030d2
rusualin0031 <123866644+rusualin0031@users.noreply.github.com>
2024-01-07 09:57:25 +0100

    Whatgpt3 Styling container

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import Feature from '../../components/feature/Feature';
 import './whatGPT3.css';
 
 const WhatGPT3 = () => {
   return (
-    <div>
-      WhatGPT3
+    <div className="gpt3__whatgpt3 section__margin" id="whpt3">
+      <div className="gpt3__whatgpt3-features">
+        <Feature />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient-text">The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
     </div>
   )
 }
